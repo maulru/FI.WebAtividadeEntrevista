4b57852f05b6a86a858b39112e8088ecba8f6a51
Mauro <maurojr_@outlook.com>
2023-12-09 00:04:11 -0300

    Inclusão do novo campo 'CPF' e máscara no campo.

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -14,7 +14,8 @@ $(document).ready(function () {
                 "Estado": $(this).find("#Estado").val(),
                 "Cidade": $(this).find("#Cidade").val(),
                 "Logradouro": $(this).find("#Logradouro").val(),
-                "Telefone": $(this).find("#Telefone").val()
+                "Telefone": $(this).find("#Telefone").val(),
+                "CPF": $(this).find("#CPF").val()
             },
             error:
             function (r) {
@@ -55,4 +56,58 @@ function ModalDialog(titulo, texto) {
 
     $('body').append(texto);
     $('#' + random).modal('show');
+}
+
+function mask(o, f) {
+    1v_obj = o
+    v_fun = f
+    setTimeout("execmask()", 1)
+}
+
+function execmask() {
+    v_obj.value = v_fun(v_obj.value)
+}
+
+function masktel(v) {
+    v = v.replace(/\D/g, "");
+    v = v.replace(/^(\d{2})(\d)/g, "($1) $2");
+    v = v.replace(/(\d)(\d{4})$/, "$1-$2");
+    return v;
+}
+
+function maskcpf(v) {
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    return v;
+}
+
+function idcss(el) {
+    return document.getElementById(el);
+}
+
+window.onload = function () {
+
+    //CELULAR -------
+    idcss('Telefone').setAttribute('maxlength', 15);
+    idcss('Telefone').onkeypress = function () {
+        mask(this, masktel);
+    }
+    idcss('Telefone').onpaste = function () {
+        mask(this, masktel);
+    }
+    //-------------
+
+
+    //CPF ---------
+    idcss('CPF').setAttribute('maxlength', 14);
+    idcss('CPF').onkeypress = function () {
+        mask(this, maskcpf);
+    }
+    idcss('CPF').onpaste = function () {
+        mask(this, maskcpf);
+    }
+    //-------------
+
 }
